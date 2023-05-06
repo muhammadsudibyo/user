7f9bf705263cc317aa9d1873f9ca395cfe4ad7dc
Ibnu Gunawan <ibnu099@gmail.com>
2023-05-06 17:34:17 +0700

    Add Payment

--- src/main/java/com/ecommerce/user/repo/AccountMasterRepo.java
+++ src/main/java/com/ecommerce/user/repo/AccountMasterRepo.java
@@ -1,7 +1,9 @@
 package com.ecommerce.user.repo;
 
 import com.ecommerce.user.model.AccountMaster;
+import com.ecommerce.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountMasterRepo extends JpaRepository<AccountMaster, Integer> {
+    AccountMaster findAccountMasterByAccountNo(String acctNo);
 }
