a99ceec640b8bf351e0fd95bf1a88a0041ffc8e7
U071776 <muhammad_sudibyo@bca.co.id>
2023-05-06 18:54:34 +0700

    UPDATE

--- src/main/java/com/ecommerce/user/service/PaymentService.java
+++ src/main/java/com/ecommerce/user/service/PaymentService.java
@@ -1,16 +1,23 @@
 package com.ecommerce.user.service;
 
+import com.ecommerce.user.kafka.KafkaProducerSystem;
 import com.ecommerce.user.model.AccountMaster;
+import com.ecommerce.user.model.PayTxnDTO;
 import com.ecommerce.user.model.User;
 import com.ecommerce.user.repo.AccountMasterRepo;
 import com.ecommerce.user.repo.PaymentRepo;
 import com.ecommerce.user.repo.UserRepo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.AbstractAuditable_;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
+@Service
 public class PaymentService {
 
     @Autowired
@@ -22,6 +29,8 @@ public class PaymentService {
     @Autowired
     AccountMasterRepo accountMasterRepo;
 
+    @Autowired private KafkaProducerSystem sender;
+
 
     @Transactional
     public String payTransaction(String username, BigDecimal totalTxn){
@@ -30,7 +39,6 @@ public class PaymentService {
         if(user.getBalance().compareTo(totalTxn) == -1) {
             return "failed";
         }
-
         user.setBalance(user.getBalance().subtract(totalTxn));
 
         AccountMaster accountMaster = accountMasterRepo.findAccountMasterByAccountNo("00000000001");
@@ -39,8 +47,23 @@ public class PaymentService {
         return "Success";
     }
 
-    public void receiveTrx(){
+    //receive kafka
+    @KafkaListener(topics = "execute_Transaction", groupId = "test-consumer-group")
+    public void receiveTrx(String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        PayTxnDTO  payTxnDTO = objectMapper.readValue(json, PayTxnDTO.class);
+        System.out.println("disiniiiii ceeeeeeek payTxnDTO = " + payTxnDTO);
+        String status = this.payTransaction(payTxnDTO.getUsername(), payTxnDTO.getTotalTrx());
+        payTxnDTO.setStatus(status);
+        this.sendTrxStatus(payTxnDTO);
+    }
 
+    //send kafka
+    public void sendTrxStatus(PayTxnDTO payTxnDTO) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(payTxnDTO);
+        System.out.println("disiniiiii seeeeeeeeend payTxnDTO = " + payTxnDTO);
+        sender.send("status_Transaction", json);
     }
 
 
